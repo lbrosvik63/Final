1e9fb2fa441ec7a258d92bd46079e1bfd12f6469
Mark Yevdash <markyev22@gmail.com>
2014-03-08 18:22:23 -0800

    Item Images and Key
    All items and key should now have their own images

--- src/useableitem/ItemFound.java
+++ src/useableitem/ItemFound.java
@@ -14,8 +14,16 @@ import javax.swing.ImageIcon;
 
 public class ItemFound {
 
-	Image itemFound = getImage("/data/item_found.png");
+	//Image itemFound = getImage("/data/item_found.png");
 	Image keyFound = getImage("/data/key_found.png");
+	Image snowDay = getImage("/data/snowday_item.png");
+	Image virus = getImage("/data/virus_item.png");
+	Image redbull = getImage("/data/redbull_item.png");
+	Image redbull4pack = getImage("/data/redbull4pack_item.png");
+	Image reverseCompiler = getImage("/data/reversecompiler_item.png");
+	Image poisenedapple = getImage("/data/poisenedapple_item.png");
+	Image coffee = getImage("/data/coffee_found.png");
+	Image fastfood = getImage("/data/fastfood_item.png");
 	
 
 	public void render(Graphics g, String item) {
@@ -27,9 +35,24 @@ public class ItemFound {
 		
 		if(item.equalsIgnoreCase("Key")){
 			g.drawImage(keyFound,0,0,null);
-		}else{
-			g.drawImage(itemFound, 0, 0,null);
+		}else if(item.equalsIgnoreCase("snow day")){
+			g.drawImage(snowDay, 0, 0,null);
+		}else if(item.equalsIgnoreCase("virus")){
+			g.drawImage(virus, 0, 0,null);
+		}else if(item.equalsIgnoreCase("redbull")){
+			g.drawImage(redbull, 0, 0,null);
+		}else if(item.equalsIgnoreCase("red bull four pack")){
+			g.drawImage(redbull4pack, 0, 0,null);
+		}else if(item.equalsIgnoreCase("reverse compiler")){
+			g.drawImage(reverseCompiler, 0, 0,null);
+		}else if(item.equalsIgnoreCase("poisened apple")){
+			g.drawImage(poisenedapple, 0, 0,null);
+		}else if(item.equalsIgnoreCase("coffee")){
+			g.drawImage(coffee, 0, 0,null);
+		}else if(item.equalsIgnoreCase("fast food")){
+			g.drawImage(fastfood, 0, 0,null);
 		}
+		
 		
 	
 	}
@@ -41,17 +64,3 @@ public class ItemFound {
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
